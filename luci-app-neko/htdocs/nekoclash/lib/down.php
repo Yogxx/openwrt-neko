6e19fd71d3d1c1b3db4476ddfc93e64286f71f51
anu <163709469+Yogxx@users.noreply.github.com>
2025-10-13 18:24:03 +0700

    Update down.php

--- luci-app-neko/htdocs/nekoclash/lib/down.php
+++ luci-app-neko/htdocs/nekoclash/lib/down.php
@@ -1,10 +1,20 @@
 <?php
-$stat = shell_exec("uci get neko.cfg.enabled");
-if($stat==1) $tmp = shell_exec("cat /sys/class/net/Meta/statistics/rx_bytes");
-else $tmp = "0";
-$data = "";
-if ($tmp < 1024000) $data = number_format(($tmp/1024),1)." KB";
-elseif ($tmp > 1024000 && $tmp < 1024000000) $data = number_format(($tmp/1024000),1)." MB";
-elseif ($tmp > 1024000000) $data = number_format(($tmp/1024000000),2)." GB";
+$stat = trim(shell_exec("uci get neko.cfg.enabled"));
+if ($stat == "1") {
+    $tmp = intval(trim(shell_exec("cat /sys/class/net/Meta/statistics/rx_bytes")));
+} else {
+    $tmp = 0;
+}
+
+if ($tmp < 1024) {
+    $data = number_format($tmp, 0) . " B";
+} elseif ($tmp < 1048576) { // < 1MB
+    $data = number_format($tmp / 1024, 1) . " KB";
+} elseif ($tmp < 1073741824) { // < 1GB
+    $data = number_format($tmp / 1048576, 1) . " MB";
+} else {
+    $data = number_format($tmp / 1073741824, 2) . " GB";
+}
+
 echo $data;
 ?>
