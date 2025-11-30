209d75fd22621dd3d60a8648ce5a5a0eaedb419c
anu <163709469+Yogxx@users.noreply.github.com>
2025-11-30 15:09:05 +0700

    Update devinfo.php

--- luci-app-neko/htdocs/nekoclash/devinfo.php
+++ luci-app-neko/htdocs/nekoclash/devinfo.php
@@ -7,7 +7,7 @@ $devices=$dt['model'];
 // OS TYPE AND KERNEL VERSION
 $kernelv=exec("cat /proc/sys/kernel/ostype").' '.exec("cat /proc/sys/kernel/osrelease");
 $OSVer=$dt['release']['distribution']." ".$dt['release']['version'];
-
+/*
 // MEMORY INFO
 $tmpramTotal=exec("cat /proc/meminfo | grep MemTotal | awk '{print $2}'");
 $tmpramAvailable=exec("cat /proc/meminfo | grep MemAvailable | awk '{print $2}'");
@@ -15,7 +15,7 @@ $tmpramAvailable=exec("cat /proc/meminfo | grep MemAvailable | awk '{print $2}'"
 $ramTotal=number_format(($tmpramTotal/1000),1);
 $ramAvailable=number_format(($tmpramAvailable/1000),1);
 $ramUsage=number_format((($tmpramTotal-$tmpramAvailable)/1000),1);
-
+*/
 // UPTIME
 $raw_uptime=exec("cat /proc/uptime | awk '{print $1}'");
 $hours = floor($raw_uptime / 3600);
